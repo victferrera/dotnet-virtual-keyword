bd603a8aec65bb37fe226dd837050b57011dc275
Vitor Ferreira <victor-bash2@hotmail.com>
2024-12-02 23:29:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace VirtualKeywordLearning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DerivedClass derivedClass = new DerivedClass();
+        derivedClass.Hello("Brian");
+
+        DerivedClassWithChanges derivedClassWithChanges = new DerivedClassWithChanges();
+        derivedClassWithChanges.Hello("John");
+    }
+}
+
+class BaseClass
+{
+    public virtual void Hello(string name)
+    {
+        Console.WriteLine($"Hello {name}");
+    }
+
+    public void NotAVirtualMethod()
+    {
+        Console.WriteLine("You cant override this method");
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    // you cant override this method
+    // no suitable method to override error will show
+    // public override void NotAVirtualMethod()
+    // {
+    //     
+    // }
+}
+
+class DerivedClassWithChanges : BaseClass
+{
+    public override void Hello(string name)
+    {
+        Console.WriteLine($"Hello {name}, welcome!");
+    }
+}
